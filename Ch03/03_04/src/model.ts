d7443c073848541de86ebf722c63b6dbb63630ab
Jess Chadwick <jesschadwick@gmail.com>
2024-02-20 14:56:58 -0500

    03_04

---
+++ Ch03/03_04/src/model.ts
@@ -0,0 +1,22 @@
+enum InventoryItemType {
+    Computer = "computer",
+    Furniture = "furniture"
+}
+
+interface InventoryItem {
+    displayName: string;
+    inventoryType: "computer" | "furniture";
+    readonly trackingNumber: string;
+    createDate: Date;
+    originalCost?: number;
+
+    addNote?: (note: string) => string;
+}
+
+function getInventoryItem(trackingNumber: string): InventoryItem {
+    return null;
+}
+
+function saveInventoryItem(item: InventoryItem) {
+}
+
